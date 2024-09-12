e19e09505d99b52d433949c30c39d24fbafb682f
gabrieldrouin <info@gabrieldrouin.com>
2024-09-12 07:25:06 -0400

    reinit

--- app.js
+++ app.js
@@ -1,56 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
-
-/**
- * -------------- GENERAL SETUP ----------------
- */
-
-// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
-
-// Create the Express application
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-/**
- * -------------- SESSION SETUP ----------------
- */
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// TODO
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-/**
- * -------------- PASSPORT AUTHENTICATION ----------------
- */
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-/**
- * -------------- ROUTES ----------------
- */
-
-// Imports all of the routes from ./routes/index.js
-app.use(routes);
-
-
-/**
- * -------------- SERVER ----------------
- */
-
-// Server listens on http://localhost:3000
-app.listen(3000);
+module.exports = app;
